282f88fed265f62d0b63bc2fa2f184b752a1fc2f
jaalonge1 <josh.alonge@digital-clarity.io>
2021-07-08 23:05:13 -0400

    Initial Commit

---
+++ Packed/PackedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackedApp.swift
+//  Packed
+//
+//  Created by Josh Alonge on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct PackedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
