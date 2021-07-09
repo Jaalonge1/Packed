24a7b4151522ce2a08789b7947e688acf2e44a2d
jaalonge1 <josh.alonge@digital-clarity.io>
2021-07-09 00:24:24 -0400

    Added fonts and basic text

--- Packed/ContentView.swift
+++ Packed/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+            Text("July")
+                .font(.custom("Lato-Bold.ttf", size: 40))
+//            Text("JULY")
+//                .font(Font.custom("Raleway-ExtraLight", size: 35))
+//            Text("18th")
+//                .font(Font.custom("Raleway-ExtraLight", size: 50))
+        
     }
 }
 
